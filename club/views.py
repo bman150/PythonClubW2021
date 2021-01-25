3d52123bdbbe008b82aea4097a113926bd58334f
bman150 <63990801+bman150@users.noreply.github.com>
2021-01-25 15:02:56 -0800

    Assignment 5 commit

--- club/views.py
+++ club/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from  .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def resources(request):
+    resource_list_1=Resource.objects.all()
+    resource_list_2=Meeting.objects.all()
+    return render(request, 'club/resources.html', {'resource_list_1':resource_list_1, 'resource_list_2':resource_list_2})
